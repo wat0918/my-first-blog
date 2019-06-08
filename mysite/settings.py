1429e9a3338736ddaef83a1627752b5c4ed03bfa
Your Name <you@example.com>
2019-06-09 02:56:45 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q-av=kjj6cux2kdbwb+=$%j4tk=p06#*1_!&$-8@3en#(jyxh^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 DEBUG = False
